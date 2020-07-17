a581a956c218d7b8a820975b39cba620c303bd3e
Rohit Saini <sketchlearn7@gmail.com>
2020-07-17 16:25:21 +0530

    Code updated

--- SwiftUI12/ContentView.swift
+++ SwiftUI12/ContentView.swift
@@ -10,12 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView{
+            page1().tabItem{
+                VStack{
+                Text("House")
+                Image(systemName: "house.fill")
+                }
+                
+            }
+            page2().tabItem{
+                VStack{
+                Text("Fire")
+                Image(systemName: "flame.fill")
+                }
+            }
+            page3().tabItem{
+                VStack{
+                Text("Share")
+                Image(systemName: "paperplane.fill")
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct page1:View{
+    var body: some View {
+        Text("Page1").fontWeight(.heavy)
+    }
+}
+
+struct page2:View{
+    var body: some View {
+        Text("Page2").fontWeight(.heavy)
+    }
+}
+struct page3:View{
+    var body: some View {
+        Text("Page3").fontWeight(.heavy)
     }
 }
