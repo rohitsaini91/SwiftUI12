a16cab3fced430fbf180f53893dfb8e55e6e2085
Rohit Saini <sketchlearn7@gmail.com>
2020-07-17 15:51:57 +0530

    Initial Commit

---
+++ SwiftUI12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI12
+//
+//  Created by Rohit Saini on 17/07/20.
+//  Copyright © 2020 AccessDenied. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
